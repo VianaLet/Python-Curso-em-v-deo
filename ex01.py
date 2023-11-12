ffaf44bb91d07d201af78484922d66f418bd6b2f
Letícia Viana <145235850+VianaLet@users.noreply.github.com>
2023-11-11 22:05:50 -0300

    criando variáveis

--- ex01.py
+++ ex01.py
@@ -1,2 +1,15 @@
 print('Hello, world')
-input('Digite seu nome: ')
+nome = input('Digite seu nome: ')  # Criando a variável nome
+idade = 23
+
+print(nome, idade)
+"""
+Tipo de váriaveis
+1. int: número inteiros, sem parte decimal: 0 5 -1 1000
+2. float: números reais, números com parte decimal, negativos ou inteiros: 1.0 -2.7
+3. str: cadeias de caracteres, ou seja, dados textuais (string)
+4. bool: valores lógicos (booleanos): True ou False
+"""
+print(type(idade))  # Imprime o tipo da variável idade
+linguagem = input('Qual é a linguagem de programação que você está estudando?')
+print('A linguagem que você está estudando é', linguagem)
