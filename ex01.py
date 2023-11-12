6f9e524102d39d1076f316bb558c65c8adcda3e8
Letícia Viana <145235850+VianaLet@users.noreply.github.com>
2023-11-11 22:25:07 -0300

    operadore aritméticos e bool

--- ex01.py
+++ ex01.py
@@ -13,3 +13,24 @@ Tipo de váriaveis
 print(type(idade))  # Imprime o tipo da variável idade
 linguagem = input('Qual é a linguagem de programação que você está estudando?')
 print('A linguagem que você está estudando é', linguagem)
+
+# > Operadores artiméticos
+"""
+- Soma: +
+- Subtração: -
+- Multiplicação: *
+- Divisão: /
+- Divisão inteira: //
+- Resto da divisão: %
+- Potencia: **
+
+"""
+# > Operadores Booleanos
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 2.0
+print(idade1 > idade2)  # Maior que
+print(idade1 <= idade2) # Menor ou igual a
+print('Python' == 'python')  # Igual a
+print('Python' != 'Abaxi')  # Diferente de
