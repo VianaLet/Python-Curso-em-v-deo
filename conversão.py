0aa646ef61c938f29ef74c6f24aa47ccc01f3ce6
Letícia Viana <145235850+VianaLet@users.noreply.github.com>
2023-11-11 22:32:59 -0300

    criando um arquivo com exemplos de conversão de tipos primitivos

---
+++ conversão.py
@@ -0,0 +1,12 @@
+# Conversão de tipos
+idade ='26'
+idade_inteira = int(idade)
+print(type(idade), type(idade_inteira))
+
+# int() vai converter para um tipo inteiro
+# str() vai converter para um tipo texto
+# float() Vai converter para um tipo float
+# bool() Vai converter para um tipo booleano
+Altura = input('Informe sua altura: ')
+altura = float(input('Informe sua altura: '))  #A qui estarei altomaticamente convertendo o valor digitado para float
+print(type(Altura), type(altura))
