86438c00e1ee649ef2f269b01e315ab43dc04396
Letícia Viana <145235850+VianaLet@users.noreply.github.com>
2023-11-24 19:40:47 -0300

    Fazendo validação de dados caso maxqtd_y > maxqtd_x

--- linha-orçamento.py
+++ linha-orçamento.py
@@ -9,16 +9,28 @@ lista_qtd_x = []
 lista_qtd_y = []
 total = 0
 print(maxqtd_x, maxqtd_y)
-for i in range (maxqtd_x + 1):
-    for j in range (maxqtd_x+ 1):    # Aqui vamos fazer os calculos com todos os números até 
-        qtd_x = i                 # Chegar na máxima de x e y, calculamos anteriormente para
-        qtd_y = j                      # Não precisar colocar o for com números imensos  
-        total = px*qtd_x + py*qtd_y
-        if (total == renda):            # Selecionando o limite, que é a renda do consumidor
-            lista_qtd_x.append(qtd_x)   # Estamos fazendo uma seleção dessas quantidades à serem
-            lista_qtd_y.append(qtd_y)   # inseridas nas listas, para então podermos vizualizá-las
-            print(f"Preço {produto_x}: {px} Quantidade: {lista_qtd_x}")
-            print(f"Preço {produto_y}: {py} Quantidade: {lista_qtd_y}\n  Total: {total}")
+if (maxqtd_x > maxqtd_y):          # Fazendo uma pequena validação, pois se o maxqtd de y for maior o resultado final seria errado
+    for i in range (maxqtd_x + 1):
+        for j in range (maxqtd_x + 1):    # Aqui vamos fazer os calculos com todos os números até 
+            qtd_x = i                     # Chegar na máxima de x +1(pois o range vai até < maxqtd), calculamos anteriormente para
+            qtd_y = j                     # Não precisar colocar o for com números imensos  
+            total = px*qtd_x + py*qtd_y
+            if (total == renda):            # Selecionando o limite, que é a renda do consumidor
+                lista_qtd_x.append(qtd_x)   # Estamos fazendo uma seleção dessas quantidades à serem
+                lista_qtd_y.append(qtd_y)   # inseridas nas listas, para então podermos vizualizá-las
+                print(f"Preço {produto_x}: {px} Quantidade: {lista_qtd_x}")
+                print(f"Preço {produto_y}: {py} Quantidade: {lista_qtd_y}\n  Total: {total}")
+else:
+    for i in range (maxqtd_y + 1):
+        for j in range (maxqtd_y + 1):    # Aqui vamos fazer os calculos com todos os números até 
+            qtd_x = i                     # Chegar na máxima de y +1(pois o range vai até < maxqtd), calculamos anteriormente para
+            qtd_y = j                     # Não precisar colocar o for com números imensos  
+            total = px*qtd_x + py*qtd_y
+            if (total == renda):            # Selecionando o limite, que é a renda do consumidor
+                lista_qtd_x.append(qtd_x)   # Estamos fazendo uma seleção dessas quantidades à serem
+                lista_qtd_y.append(qtd_y)   # inseridas nas listas, para então podermos vizualizá-las
+                print(f"Preço {produto_x}: {px} Quantidade: {lista_qtd_x}")
+                print(f"Preço {produto_y}: {py} Quantidade: {lista_qtd_y}\n  Total: {total}")
 """
 O que irá aparecer no console:
 Preço Arroz: 2.0 Quantidade: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
