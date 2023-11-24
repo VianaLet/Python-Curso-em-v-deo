18760478160b36f801f4f08526a0dc4be0113d30
Letícia Viana <145235850+VianaLet@users.noreply.github.com>
2023-11-24 16:20:49 -0300

    Update linha-orçamento.py

--- linha-orçamento.py
+++ linha-orçamento.py
@@ -2,5 +2,17 @@ produto_x = input("Qual é o produto x?")
 px = int(input(f"Informe o preço do(a) {produto_x}: "))
 produto_y = input("Qual é o produto y?")
 py = int(input(f"Informe o preço do(a) {produto_y}: "))
-qtd_x = []
-qtd_y = []
+i = 1
+qtd_x = 1
+qtd_y = [0]
+totalx = px*qtd_x
+totaly = px*qtd_y
+
+print(totalx)
+while (totalx <= 100):
+    qtd_x = i
+    print(qtd_x)
+    print(totalx)
+    i = i + 1
+if (totalx == 100):
+    print(totalx)
