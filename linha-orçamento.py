7aa06416888ce51d4cb9e8b0eb4b55384b9a3873
Letícia Viana <145235850+VianaLet@users.noreply.github.com>
2023-11-24 14:48:34 -0300

    Iniciando um novo projeto para calcular fronteiras de gastos

---
+++ linha-orçamento.py
@@ -0,0 +1,6 @@
+produto_x = input("Qual é o produto x?")
+px = int(input(f"Informe o preço do(a) {produto_x}: "))
+produto_y = input("Qual é o produto y?")
+py = int(input(f"Informe o preço do(a) {produto_y}: "))
+qtd_x = []
+qtd_y = []
